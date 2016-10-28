1f7c2b2759fc82da5a7842d5c19efa6ba8a22fc6
LIU Xiyuan <xliuba@connect.ust.hk>
2016-10-28 16:47:37 +0800

    Update control_function.h

--- proj1phase3/your_work/control_function.h
+++ proj1phase3/your_work/control_function.h
@@ -35,17 +35,11 @@ void SO3Control_function( const double des_pos[3],
                           double &target_thrust
                         )
 {
- //       std::cout << "my control function " << std::endl;
-
    	target_thrust=Mass*(Gravity+Kd[2]*(des_vel[2]-now_vel[2])+Kp[2]*(des_pos[2]-now_pos[2]));
 	double rdd_des = Kd[0]*(des_vel[0]-now_vel[0])+Kp[0]*(des_pos[0]-now_pos[0]);
 	double pdd_des = Kd[1]*(des_vel[1]-now_vel[1])+Kp[1]*(des_pos[1]-now_pos[1]);
 	rpy[0]=1/Gravity*(rdd_des*sin(des_yaw)-pdd_des*cos(des_yaw));
 	rpy[1]=1/Gravity*(rdd_des*cos(des_yaw)+pdd_des*sin(des_yaw));
 	rpy[2]=des_yaw;
-      
-      // std::cout << "ROLL:   "<<rpy[0]<<std::endl;
-      //  std::cout<< "PITCH:   "<<rpy[1]<<std::endl;
-//        std::cout << "YAW:   "<<rpy[2]<<std::endl;
 }
 #endif
